15db832a44d846aa27177bd8bb006417d87b5fd0
vinni-lacerda <vini.lacerda@hotmail.com>
2026-04-16 18:41:04 -0300

    chore: configuring enviroment

---
+++ backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory(8)->create([
+            // 'name' => 'Test User',
+            // 'email' => 'test@example.com',
+            'date_of_birth' => Carbon::now()->subYears(20)->format('Y-m-d')
+        ]);
+    }
+}
