8e18975d3f6e44ae7c2841fea88eed82a3204575
vinni-lacerda <vini.lacerda@hotmail.com>
2026-04-17 17:02:34 -0300

    refactor: routing all http request typs with only one class

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']);
+// Route::get('/users', [UserController::class, 'index']);
+// Route::post('/users', [UserController::class, 'store']);
+Route::apiResource('/users', UserController::class);
