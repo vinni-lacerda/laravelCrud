4271d36db8b46d59ed168de584f49ab2b9636ed8
vinni-lacerda <vini.lacerda@hotmail.com>
2026-04-20 12:55:21 -0300

    refactor:  adding 'fillable' and 'hidden' to protect data and control API responsiveness.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,29 +4,30 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'date_of_birth'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected array $fillable = [
+        'name',
+        'email',
+        'password',
+        'date_of_birth',
+    ];
+
+    protected array $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected array $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'date_of_birth' => 'date',
+    ];
 }
