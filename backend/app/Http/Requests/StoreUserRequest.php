0c10742a603e0283ad00f0f9dc7c0596b19ac71a
vinni-lacerda <vini.lacerda@hotmail.com>
2026-04-20 17:39:59 -0300

    refactor: Change password validation to be optional in StoreUserRequest.

--- backend/app/Http/Requests/StoreUserRequest.php
+++ backend/app/Http/Requests/StoreUserRequest.php
@@ -22,7 +22,7 @@ class StoreUserRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['nullable', 'string', 'min:8', 'confirmed'],
             'date_of_birth' => ['nullable', 'date', 'before:today'],
         ];
     }
